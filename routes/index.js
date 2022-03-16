a4648732973b04e23bc01e50d9944efd6367b5d2
vutat.thanh@etc.vn <vutatthanh@96>
2022-03-16 15:57:14 +0700

    rename project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const userRoutes = require('./users');
+
+const app = new express();
+
+app.use('fb-comment', userRoutes)
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/webhook', function(req, res, next) {
+  if (req.query['hub_verify_token'] === 'thanhvt') {
+    console.log(req.body)
+    res.send(req.query['hub_challenge']);
+  } else {
+    res.send('nothing');
+  }
+});
+
+router.post('/webhook', function(req, res, next) {
+  if (req.query['hub_verify_token'] === 'thanhvt') {
+    console.log(req.body);
+    res.send(req.query['hub_challenge']);
+  } else {
+    res.send('nothing');
+  }
 });
 
 module.exports = router;
+
