60643c1c484998346d0c8aa647d89be63f8862e1
thanhvt <thanhvt@vissoft.vn>
2022-03-16 19:11:22 +0700

    update 2

--- routes/fb-comment.js
+++ routes/fb-comment.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/webhook', function(req, res, next) {
-  if (req.query['hub_verify_token'] === 'thanhvt') {
+  if (req.query['hub.verify_token'] === 'thanhvt') {
     console.log(req.body)
-    res.send(req.query['hub_challenge']);
+    res.send(req.query['hub.challenge']);
   } else {
     res.send('nothing');
   }
 });
 
 router.post('/webhook', function(req, res, next) {
-  if (req.query['hub_verify_token'] === 'thanhvt') {
+  if (req.query['hub.verify_token'] === 'thanhvt') {
     console.log(req.body);
-    res.send(req.query['hub_challenge']);
+    res.send(req.query['hub.challenge']);
   } else {
     res.send('nothing');
   }
